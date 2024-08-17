1a839a20c50315e6582f07ec421f0c43c423d2d0
Aureli <aureli.roura@gmail.com>
2024-08-17 19:20:08 +0200

    add up and down moving steps

--- frontend/src/components/EditSteps.vue
+++ frontend/src/components/EditSteps.vue
@@ -10,44 +10,44 @@
           <v-chip @click="addStep" :disabled="!valid" color="primary">Afegir Pas</v-chip>
         </v-col>
       </v-row>
-      <v-list class="list-container flex-grow-1">
-        <v-list-item v-for="(step, index) in steps" :key="index" density="compact">
-          <div class="d-flex justify-space-between">
-            <template v-if="editIndex === index">
-              <v-row>
-                <v-col cols="12" md="9">
-                  <v-textarea v-model="editedStep" label="Edita Pas" dense></v-textarea>
-                </v-col>
-                <v-col cols="12" md="3">
-                  <v-chip @click="cancelEdit" color="red" class="ml-2">Cancel</v-chip>
-                  <v-chip @click="saveStep(index)" color="primary" class="ml-2">Save</v-chip>
-                </v-col>
-              </v-row>
-            </template>
-            <template v-else>
-              {{ index + 1 }}. {{ step }}
-              <v-list-item-action>
-                <v-icon color="blue" @click="editStep(index, step)">mdi-pencil</v-icon>
-                <v-icon color="red" @click="removeStep(index)" class="ml-4">mdi-delete</v-icon>
-              </v-list-item-action>
-            </template>
-          </div>
-        </v-list-item>
-      </v-list>
+        <v-list class="list-container flex-grow-1">
+          <v-list-item v-for="(step, index) in steps" :key="index" density="compact">
+            <div class="d-flex justify-space-between">
+              <template v-if="editIndex === index">
+                <v-row>
+                  <v-col cols="12" md="9">
+                    <v-textarea v-model="editedStep" label="Edita Pas" dense></v-textarea>
+                  </v-col>
+                  <v-col cols="12" md="3">
+                    <v-chip @click="cancelEdit" color="red" class="ml-2">Cancel</v-chip>
+                    <v-chip @click="saveStep(index)" color="primary" class="ml-2">Save</v-chip>
+                  </v-col>
+                </v-row>
+              </template>
+              <template v-else>
+                {{ index + 1 }}. {{ step }}
+                <v-list-item-action>
+                  <v-icon :disabled="index == 0" color="green" @click="upStep(index)">mdi-arrow-up</v-icon>
+                  <v-icon :disabled="index == steps.length - 1"  color="green" @click="downStep(index)" class="mr-4">mdi-arrow-down</v-icon>
+                  <v-icon color="blue" @click="editStep(index, step)">mdi-pencil</v-icon>
+                  <v-icon color="red" @click="removeStep(index)" class="ml-4">mdi-delete</v-icon>
+                </v-list-item-action>
+              </template>
+            </div>
+          </v-list-item>
+        </v-list>
     </v-card-text>
   </v-card>
 </template>
 
 <script setup>
-import { ref, reactive, watch, computed, toRef } from 'vue';
-import { useCategoriesStore } from '@/stores/categoriesStore';
+import { ref, watch, computed, toRef } from 'vue';
 import { addMessage } from '@/modules/arrMessage';
-
-const categoriesStore = useCategoriesStore();
 
 const props = defineProps({
   steps: Array
 });
+
 
 let steps = toRef(props, 'steps');
 
@@ -101,6 +101,21 @@ const cancelEdit = () => {
   editedStep.value = '';
 };
 
+const upStep = (index) => {
+  if (index > 0) {
+    const temp = steps.value[index];
+    steps.value[index] = steps.value[index - 1];
+    steps.value[index - 1] = temp;
+  }
+};
+
+const downStep = (index) => {
+  if (index < steps.value.length - 1) {
+    const temp = steps.value[index];
+    steps.value[index] = steps.value[index + 1];
+    steps.value[index + 1] = temp;
+  }
+};
 </script>
 
 <style scoped>
