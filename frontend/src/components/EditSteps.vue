729b250fc31209e4809a0c25ff27d705f5ae2ea7
Aureli <aureli.roura@gmail.com>
2024-09-16 18:28:48 +0200

    Hide headers where not necessari and change scrolls

--- frontend/src/components/EditSteps.vue
+++ frontend/src/components/EditSteps.vue
@@ -1,8 +1,19 @@
 <template>
   <v-card>
-    <v-card-title>Passos</v-card-title>
+    <v-card-title>Passos
+      <v-tooltip text="Edita Passos">
+        <template v-slot:activator="{ props }">
+          <v-btn icon size="x-small" density="comfortable" @click="selectionActive = !selectionActive" color="primary"
+            class="ml-1" v-bind="props">
+            <v-icon v-if="!selectionActive">mdi-plus</v-icon>
+            <v-icon v-else>mdi-minus</v-icon>
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </v-card-title>
     <v-card-text>
-      <v-row>
+
+      <v-row v-if="selectionActive">
         <v-col cols="12" md="10">
           <v-textarea v-model="newStep" label="Pas" required></v-textarea>
         </v-col>
@@ -10,32 +21,33 @@
           <v-chip @click="addStep" :disabled="!valid" color="primary">Afegir Pas</v-chip>
         </v-col>
       </v-row>
-        <v-list class="list-container flex-grow-1">
-          <v-list-item v-for="(step, index) in steps" :key="index" density="compact">
-            <div class="d-flex justify-space-between">
-              <template v-if="editIndex === index">
-                <v-row>
-                  <v-col cols="12" md="9">
-                    <v-textarea v-model="editedStep" label="Edita Pas" density="compact"></v-textarea>
-                  </v-col>
-                  <v-col cols="12" md="3">
-                    <v-chip @click="cancelEdit" color="red" class="ml-2">Cancel</v-chip>
-                    <v-chip @click="saveStep(index)" color="primary" class="ml-2">Save</v-chip>
-                  </v-col>
-                </v-row>
-              </template>
-              <template v-else>
-                {{ index + 1 }}. {{ step }}
-                <v-list-item-action>
-                  <v-icon :disabled="index == 0" color="green" @click="upStep(index)">mdi-arrow-up</v-icon>
-                  <v-icon :disabled="index == steps.length - 1"  color="green" @click="downStep(index)" class="mr-4">mdi-arrow-down</v-icon>
-                  <v-icon color="blue" @click="editStep(index, step)">mdi-pencil</v-icon>
-                  <v-icon color="red" @click="removeStep(index)" class="ml-4">mdi-delete</v-icon>
-                </v-list-item-action>
-              </template>
-            </div>
-          </v-list-item>
-        </v-list>
+      <v-list class="list-container flex-grow-1">
+        <v-list-item v-for="(step, index) in steps" :key="index" density="compact">
+          <div class="d-flex justify-space-between">
+            <template v-if="editIndex === index">
+              <v-row>
+                <v-col cols="12" md="9">
+                  <v-textarea v-model="editedStep" label="Edita Pas" density="compact"></v-textarea>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <v-chip @click="cancelEdit" color="red" class="ml-2">Cancel</v-chip>
+                  <v-chip @click="saveStep(index)" color="primary" class="ml-2">Save</v-chip>
+                </v-col>
+              </v-row>
+            </template>
+            <template v-else>
+              {{ index + 1 }}. {{ step }}
+              <v-list-item-action v-if="selectionActive">
+                <v-icon :disabled="index == 0" color="green" @click="upStep(index)">mdi-arrow-up</v-icon>
+                <v-icon :disabled="index == steps.length - 1" color="green" @click="downStep(index)"
+                  class="mr-4">mdi-arrow-down</v-icon>
+                <v-icon color="blue" @click="editStep(index, step)">mdi-pencil</v-icon>
+                <v-icon color="red" @click="removeStep(index)" class="ml-4">mdi-delete</v-icon>
+              </v-list-item-action>
+            </template>
+          </div>
+        </v-list-item>
+      </v-list>
     </v-card-text>
   </v-card>
 </template>
@@ -55,6 +67,7 @@ let steps = toRef(props, 'steps');
 let newStep = ref('');
 const editIndex = ref(-1);
 let editedStep = ref('');
+const selectionActive = ref(false);
 
 const emit = defineEmits(['update:steps']);
 
