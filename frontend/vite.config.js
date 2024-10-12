4cddfd01e84acca43656d791aad346d3ea44a14a
Aureli <aureli.roura@gmail.com>
2024-10-12 10:53:44 +0200

    add vite dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,28 @@ export default defineConfig({
       autoImport: { labs: true }
     }),
   ],
+  optimizeDeps: {
+    include: ['vuetify/lib/components/VLayout/index.mjs',
+              'vuetify/lib/components/VContainer/index.mjs',
+              'vuetify/lib/components/VBtn/index.mjs',
+              'vuetify/lib/components/VIcon/index.mjs',
+              'vuetify/lib/components/VTable/index.mjs',
+              'vuetify/lib/components/transitions/index.mjs',
+              'vuetify/lib/components/VProgressLinear/index.mjs',
+              'vuetify/lib/components/VTextField/index.mjs',
+              'vuetify/lib/components/VCard/index.mjs',
+              'vuetify/lib/components/VAutocomplete/index.mjs',
+              'vuetify/lib/components/VChip/index.mjs', 
+              'vuetify/lib/components/VTooltip/index.mjs', 
+              'vuetify/lib/components/VSelect/index.mjs', 
+              'vuetify/lib/components/VSheet/index.mjs', 
+              'vuetify/lib/components/VTabs/index.mjs',
+              'vuetify/lib/components/VTextarea/index.mjs',
+              'vuetify/lib/components/VResponsive/index.mjs',
+              'vuetify/lib/components/VForm/index.mjs', 
+              'vuetify/lib/components/VToolbar/index.mjs',
+    ],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
