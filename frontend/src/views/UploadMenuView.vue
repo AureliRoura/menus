566a0563615da6e8aa6464436b655edaf60f2df2
Aureli <aureli.roura@gmail.com>
2024-10-20 08:33:36 +0200

    change menu order

--- frontend/src/views/UploadMenuView.vue
+++ frontend/src/views/UploadMenuView.vue
@@ -1,27 +1,24 @@
 <template>
-    <v-card class="mt-2">
-      <v-tabs
-        v-model="tab"
-        bg-color="secondary"
-      >
-        <v-tab value="one">Menus</v-tab>
-        <v-tab value="two">Receptes</v-tab>
-      </v-tabs>
-  
-      <v-card-text>
-        <v-tabs-window v-model="tab">
-          <v-tabs-window-item value="one">
-            <upload-menu />
-          </v-tabs-window-item>
-  
-          <v-tabs-window-item value="two">
-            <upload-recipe />
-          </v-tabs-window-item>
-  
-        </v-tabs-window>
-      </v-card-text>
-    </v-card>
-  </template>
+  <v-card class="mt-2">
+    <v-tabs v-model="tab" bg-color="secondary">
+      <v-tab value="two">Receptes</v-tab>
+      <v-tab value="one">Menus</v-tab>
+    </v-tabs>
+
+    <v-card-text>
+      <v-tabs-window v-model="tab">
+        <v-tabs-window-item value="one">
+          <upload-menu />
+        </v-tabs-window-item>
+
+        <v-tabs-window-item value="two">
+          <upload-recipe />
+        </v-tabs-window-item>
+
+      </v-tabs-window>
+    </v-card-text>
+  </v-card>
+</template>
 
 <script setup>
 import { ref } from 'vue';
@@ -29,4 +26,4 @@ import UploadMenu from '@/components/UploadMenu.vue';
 import UploadRecipe from '@/components/UploadRecipe.vue';
 
 const tab = ref(null);
-</script>  
+</script>
